6af64663138500c8ec4814e05810242b989266c6
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2017-03-12 12:59:32 +0000

    remove old src_sql command (replaced by src_dbi), added sql_escape ident and string and uncomment copy_to

--- R/dplyr.R
+++ R/dplyr.R
@@ -1,5 +1,9 @@
+#' @import methods
+#' @import assertthat
+#' @importFrom dplyr src_sql
+#' @importFrom DBI dbConnect
 #' @export
-src_clickhouse <- function(dbname = "", host = "localhost", port = 8123L, user = "",
+src_clickhouse <- function(dbname = "default", host = "localhost", port = 8123L, user = "default",
                            password = "", ...) {
   if (!requireNamespace("dplyr", quietly = TRUE)) {
     stop("dplyr is required to use src_clickhouse", call. = FALSE)
@@ -7,24 +11,7 @@ src_clickhouse <- function(dbname = "", host = "localhost", port = 8123L, user =
 
   con <- DBI::dbConnect(clckhs::Clickhouse(), host = host, dbname = dbname,
                         user = user, password = password, port = port, ...)
-
-  #src_sql("clickhouse", con)
   src_dbi(con)
-}
-
-
-# No transactions supported so
-# we have torewrite copy_to
-#' @export
-copy_to.src_clickhouse <- function(dest, df, name = deparse(substitute(df)),
-                                   overwrite = FALSE, ...) {
-  dbWriteTable(dest$con, name, df, overwrite=overwrite)
-
-  #dplyr:::db_create_indexes(con, name, indexes)
-  #if (analyze)
-  #  db_analyze(con, name)
-
-  tbl(dest, name)
 }
 
 
@@ -32,6 +19,16 @@ copy_to.src_clickhouse <- function(dest, df, name = deparse(substitute(df)),
 #' @importFrom dplyr src_desc
 src_desc.src_clickhouse <- function(con) {
   paste0("clickhouse ","[", con$con@url, "]")
+}
+
+#' @export
+sql_escape_string.src_clickhouse <- function(con, x) {
+  sql_quote(x, "'")
+}
+
+#' @export
+sql_escape_ident.src_clickhouse <- function(con, x) {
+  sql_quote(x, "`")
 }
 
 db_create_table.src_clickhouse <- function(con, table, types,
@@ -44,6 +41,24 @@ db_create_table.src_clickhouse <- function(con, table, types,
     dbExecute(con$con, ct)
   }
 }
+
+# No transactions supported so
+# we have torewrite copy_to
+#importFrom dplyr copy_to
+#export
+#return a \code{tbl} object in the remote source
+#seealso \code{\link[dplyr]{copy_to}}
+#copy_to.src_clickhouse <- function(dest, df, name = deparse(substitute(df)),
+#                                   overwrite = FALSE, ...) {
+#  dbWriteTable(dest$con, name, df, overwrite=overwrite)
+#
+#  #dplyr:::db_create_indexes(con, name, indexes)
+#  #if (analyze)
+#  #  db_analyze(con, name)
+#
+#  tbl(dest, name)
+#}
+
 
 #' @export
 #' @importFrom dplyr db_explain
