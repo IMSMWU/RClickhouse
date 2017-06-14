17d4795773c4371c78b4f92df8dd546d55848d70
v5r <github@b8000h.org>
2017-06-14 12:36:43 +0200

    Squashed 'src/vendor/clickhouse-cpp/' changes from f35601a..5f39c10
    5f39c10 Merge pull request #25 from artpaul/errors
    791ea1d #24 merge with master
    15980e0 Merge pull request #22 from v5r/master
    715a557 #24 add some error codes
    dc8face add support for cancelable queries
    
    git-subtree-dir: src/vendor/clickhouse-cpp
    git-subtree-split: 5f39c1088dc52872e72c4292a027611e28027c85

--- tests/simple/main.cpp
+++ tests/simple/main.cpp
@@ -1,4 +1,5 @@
 #include <clickhouse/client.h>
+#include <clickhouse/error_codes.h>
 #include <clickhouse/types/type_parser.h>
 
 #include <iostream>
@@ -206,12 +207,63 @@ inline void NumbersExample(Client& client) {
     );
 }
 
+inline void CancelableExample(Client& client) {
+
+    /// Create a table.
+    client.Execute("CREATE TABLE IF NOT EXISTS test.client (x UInt64) ENGINE = Memory");
+
+    /// Insert a few blocks.
+    for (unsigned j = 0; j < 10; j++) {
+        Block b;
+
+        auto x = std::make_shared<ColumnUInt64>();
+        for (uint64_t i = 0; i < 1000; i++) {
+            x->Append(i);
+        }
+
+        b.AppendColumn("x", x);
+        client.Insert("test.client", b);
+    }
+
+    /// Send a query which is canceled after receiving the first block (note:
+    /// due to the low number of rows in this test, this will not actually have
+    /// any effect, it just tests for errors)
+    client.SelectCancelable("SELECT * FROM test.client", [](const Block&)
+        {
+            return false;
+        }
+    );
+
+    /// Delete table.
+    client.Execute("DROP TABLE test.client");
+}
+
+inline void ExecptionExample(Client& client) {
+    /// Create a table.
+    client.Execute("CREATE TABLE IF NOT EXISTS test.exceptions (id UInt64, name String) ENGINE = Memory");
+    /// Expect failing on table creation.
+    try {
+        client.Execute("CREATE TABLE test.exceptions (id UInt64, name String) ENGINE = Memory");
+    } catch (const ServerException& e) {
+        if (e.GetCode() == ErrorCodes::TABLE_ALREADY_EXISTS) {
+            // OK
+        } else {
+            throw;
+        }
+    }
+
+    /// Delete table.
+    client.Execute("DROP TABLE test.exceptions");
+}
+
 static void RunTests(Client& client) {
     ArrayExample(client);
     DateExample(client);
     GenericExample(client);
     NullableExample(client);
     NumbersExample(client);
+    CancelableExample(client);
+    ExecptionExample(client);
 }
 
 int main() {
