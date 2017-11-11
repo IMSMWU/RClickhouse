5f8be1088936fdef8049444d24941e4315ce6fcb
v5r <github@b8000h.org>
2017-11-11 18:28:43 +0100

    add regression test for #15

---
+++ tests/testthat/test-regr-15.R
@@ -0,0 +1,13 @@
+context("regr-15")
+
+library(DBI, warn.conflicts=F)
+library(dplyr, warn.conflicts=F)
+library(dbplyr, warn.conflicts=F)
+
+test_that("correct conversion of logical values in dplyr (#15)", {
+  conn <- dbConnect(clckhs::clickhouse(), host="localhost")
+  dbWriteTable(conn, "test", data.frame(x=c(20,21,22)), overwrite=T)
+  expect_equal(as.data.frame(tbl(conn, "test") %>% summarize(isGt = ifelse(x > 20, TRUE, FALSE))),
+               data.frame(isGt=c(0,1,1)))
+  dbDisconnect(conn)
+})
