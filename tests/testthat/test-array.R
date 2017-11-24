f7e9b2118f777047203b283e7d4c7b8c0bbf7d87
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2017-11-24 08:16:46 +0100

    do not run tests on cran

--- tests/testthat/test-array.R
+++ tests/testthat/test-array.R
@@ -5,6 +5,7 @@ library(dplyr, warn.conflicts=F)
 library(dbplyr, warn.conflicts=F)
 
 test_that("reading & writing array columns", {
+  skip_on_cran()
   conn <- dbConnect(RClickhouse::clickhouse(), host=serveraddr)
   df <- as.data.frame(data_frame(x=list(c(1,3,5),c(1,2))))
   dbWriteTable(conn, tblname, df, overwrite=T)
@@ -25,6 +26,7 @@ test_that("reading & writing array columns", {
 #})
 
 test_that("array columns with empty entries", {
+  skip_on_cran()
   conn <- dbConnect(RClickhouse::clickhouse(), host=serveraddr)
   df <- as.data.frame(data_frame(x=list(c(1,2,3),as.numeric(c()),c(4,5))))
   dbWriteTable(conn, tblname, df, overwrite=T)
--- tests/testthat/test-regr-15.R
+++ tests/testthat/test-regr-15.R
@@ -5,6 +5,7 @@ library(dplyr, warn.conflicts=F)
 library(dbplyr, warn.conflicts=F)
 
 test_that("correct conversion of logical values in dplyr (#15)", {
+  skip_on_cran()
   conn <- dbConnect(RClickhouse::clickhouse(), host=serveraddr)
   dbWriteTable(conn, tblname, data.frame(x=c(20,21,22)), overwrite=T)
   expect_equal(as.data.frame(tbl(conn, tblname) %>% summarize(isGt = ifelse(x > 20, TRUE, FALSE))),
