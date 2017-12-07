6cd92f0c12e13db6dec825992e182a05663fedaa
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2017-12-07 07:50:28 +0100

    nullable-column bug should be fixed now (clickhouse-cpp)

--- tests/testthat/test-array.R
+++ tests/testthat/test-array.R
@@ -9,10 +9,9 @@ test_that("reading & writing array columns", {
   writeReadTest(as.data.frame(data_frame(x=list(c(1,3,5),c(1,2)))))
 })
 
-#TODO: fix bug in clickhouse-cpp first
-#test_that("nullable array columns", {
-#  writeReadTest(as.data.frame(data_frame(x=list(c(1,NA,3,5),c(1,2,NA)))))
-#})
+test_that("nullable array columns", {
+  writeReadTest(as.data.frame(data_frame(x=list(c(1,NA,3,5),c(1,2,NA)))))
+})
 
 test_that("array columns with empty entries", {
   writeReadTest(as.data.frame(data_frame(x=list(c(1,2,3),as.numeric(c()),c(4,5)))))
