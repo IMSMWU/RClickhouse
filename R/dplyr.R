f37e4bc24a024c43477a08f383edfb876c964abd
Daniel Winkler <winkler.daniel1@gmail.com>
2018-06-11 22:08:51 +0200

    Now reassigning the original "sql_translate" function to the "dbplyr" namspace when Rclickhouse is detached. Notice that while Rclickhouse is loaded dbplyr will not perform "toupper" for unknown functions of any database. Also added testcase for case sensitive translation of unknown functions

--- R/dplyr.R
+++ R/dplyr.R
@@ -33,12 +33,13 @@ ch_sql_prefix <- function(f) {
 
 # Overwrite sql_prefix
 # Adapted from R.utils' "reassignInPackage" function
-dbpenv <- environment(dbplyr::sql_prefix)
-unlockBinding("sql_prefix", dbpenv)
-assignInNamespace("sql_prefix", ch_sql_prefix,
+curSQLprefix <- dbplyr::sql_prefix
+dbpenv <- environment(dbplyr::build_sql)
+base::unlockBinding("sql_prefix", dbpenv)
+utils::assignInNamespace("sql_prefix", ch_sql_prefix,
                   ns = "dbplyr", envir = dbpenv)
 assign("sql_prefix", ch_sql_prefix, envir = dbpenv)
-lockBinding("sql_prefix", dbpenv)
+base::lockBinding("sql_prefix", dbpenv)
 
 #' @export
 #' @importFrom dplyr db_explain
