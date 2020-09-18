20b8aa080ddf0e72748201a6b9bad23acde1d8b4
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-09-18 05:12:41 -0500

    Incorporate dbAppendTable function (#73)
    * Add dbAppendTable
    
    * Fix Syntax
    
    * Updates to add dbAppendTable and dbCreateTable
    
    * Add engine parameter
    
    * Force engine=engine
    
    * Revert back to original
    
    * Revert fix
    
    * Add dbAppendTable vanilla
    
    * Remove excess arguments
    
    * Add from dbWriteTable
    
    * Remove dbAppendTable method for now
    
    * Add dbAppendTable function
    
    * Add Test for dbAppendTable
    
    * Add row.names parameter to function call
    
    * Forgot parenthesis
    
    * Update to remove ... from arguments
    
    * Fix mistakes in value assignment
    
    * Fix my own issues with not having the correct parameter values passed

--- tests/testthat/test-testdriven_development.R
+++ tests/testthat/test-testdriven_development.R
@@ -22,6 +22,26 @@ test_that("ISSUE #71", {
   RClickhouse::dbRemoveTable(conn,"PersonalInfo")
 })
 
+test_that("ISSUE #71 dbAppendTable", {
+  conn <- getRealConnection()
+
+  tablename <- "PersonalInfo"
+  columns <- as.data.frame(data.frame('aName','Age','Profession'))
+  types <- c("String","Float64","String")
+
+  dbCreateTable(conn, tablename, fields=columns, overwrite=TRUE, field.types=types)
+
+  values <- data.frame(aName='John Smith', Age=25, Profession='Software Engineer')
+  dbAppendTable(conn, tablename, value=values, row.names=FALSE)
+
+  # after <- dbReadTable(conn, "PersonalInfo")
+
+
+
+  # expect_equal(before, after)
+  RClickhouse::dbRemoveTable(conn,"PersonalInfo")
+})
+
 
 
 
