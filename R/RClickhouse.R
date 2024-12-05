98220acc96ec66b869cbb5993782ff1e14bb7e0d
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2024-12-05 10:58:08 +0100

    update documentation

--- R/RClickhouse.R
+++ R/RClickhouse.R
@@ -3,8 +3,7 @@
 #' provides access to a clickhouse database
 #'
 #' @name RClickhouse
-#' @docType package
 #' @aliases RClickhouse-package
 #' @importFrom Rcpp evalCpp
 #' @useDynLib RClickhouse, .registration=TRUE
-NULL
+"_PACKAGE"
